709090777c2ef26a1faa03cb4cc636497c11e6fb
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-08-26 20:15:42 +0900

    4. 予約フォームを作成してみよう

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,23 @@
-<?php
-echo 'Welcome TECH QUEST!';
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>予約ページ</title>
+    </head>
+    <body>
+        <form action="complete.php" method="post">
+            予約者名
+            <input type="text" name="name" value="" /><br />
+
+            Email
+            <input type="text" name="email" value="" /><br />
+
+            電話番号
+            <input type="text" name="phone_number" value="" ><br />
+
+            <br />
+
+            <input type="submit" value="  送信" />
+        </form>
+    </body>
+</html>
